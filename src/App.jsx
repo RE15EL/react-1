b18be1f5525cc4daf9071a253a2fbe32019d539d
reisel <reiselvalle@gmail.com>
2023-09-29 23:18:22 -0400

    check winner

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,17 @@ const Square= ( {children, updateBoard, index, isSelected=false} ) => {
   )
 } 
 
-
+//winner combis
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
 
 export function App() {
   const format = (username)=>`@${username}`; //funcion que se pasa como una props
@@ -52,16 +62,52 @@ export function App() {
   //estados
   const [turn, setTurn] = useState(turns.x);
   const [board, setBoard] = useState(Array(9).fill(null));
+  const [winner, setWinner] = useState(null);
+
+  //CHECK WINNER  
+  const checkWinner = (boardToCheck) => {
+    for(const combo of WINNER_COMBOS) {
+      const [a,b,c] = combo;
+      if(
+          boardToCheck[a] && 
+          boardToCheck[a] === boardToCheck[b] && 
+          boardToCheck[a] === boardToCheck[c]
+        ) {
+        return boardToCheck[a];
+      }
+    }
+    //no winner
+    return null;
+  }
 
   //updateBoard
   const updateBoard = (index) => {
+    if (board[index] || winner) return;
     const newBoard = [...board];
     newBoard[index] = turn;
     setBoard(newBoard);
     setTurn(turn === turns.x? turns.o : turns.x);
     const newTurn = turn === turns.x ? turns.o : turns.x;
     setTurn(newTurn);
+
+    //revisar si hay ganador
+    const newWinner = checkWinner(newBoard);
+    if (newWinner) {
+      setWinner(newWinner);
+    }else if(checkEndGame(newBoard)){
+      setWinner(false);//empate
+    }
   }
+
+  //reset game
+  const resetGame = () => {
+    setWinner(null);
+    setBoard(Array(9).fill(null));
+    setTurn(turns.x);
+  }
+
+  //revisar si hay empate
+  const checkEndGame = (boardToCheck) => boardToCheck.every(cell => cell !== null);
 
   return (
     <div className="container">
@@ -87,6 +133,28 @@ export function App() {
               <Square isSelected={turn === turns.x}> {turns.x} </Square>            
               <Square isSelected={turn === turns.o}> {turns.o} </Square>           
           </div>
+
+          {
+            winner !== null && (
+              <section className='winner'>   
+                <div className="text">
+                  <h2>
+                    {
+                      winner === false
+                      ? 'Empate'  
+                      : `Ganador`
+                    }
+                  </h2>
+                  <header className="win">
+                    { winner && <Square> {winner} </Square>}
+                  </header>
+                  <footer>
+                    <button className='reset' onClick={resetGame}> Reiniciar </button>
+                  </footer>
+                </div>
+              </section>
+            )
+          }
         </section>
 
         <section className='card twitterFollowCard-container'>
